5f895053ada03c306cc3d5dbf852af829b7207ec
Madhur <you@example.com>
2025-09-28 23:38:30 +0545

    sad

--- frontend/src/utils/getProfileImageUrl.ts
+++ frontend/src/utils/getProfileImageUrl.ts
@@ -1,6 +1,6 @@
 import { BASE_URL } from "../config";
 
-const getProfileImageUrl = (profileImage: any) => {
+export const getProfileImageUrl = (profileImage: any) => {
   if (!profileImage) return `${BASE_URL}/favicon.png`;
 
   if (typeof profileImage === "object" && profileImage.url) {
